bf3aceb195e627fb08375262ba332986b501ae6c
Greg Davis <gdavis111@live.ca>
2018-02-24 19:27:41 +0000

    Cleaned up code and added screen shots for the README file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tinyApp
-Tiny App project and exercises leading up to completion
+# TinyApp Project
+
+TinyApp is a full stack web application built wit Node and Express that allows users to shorten URLs and use those URLs to access the websites they are associated with.
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+
+## Getting Started
+
+- Install all dependencies (using the 'npm install' command).
+- Run the development web server using the 'node express_server.js' command.
--- express_server.js
+++ express_server.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const cookieParser = require("cookie-parser"); // get rid of this
-const cookieSession = require('cookie-session')
-const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
-const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
+var express = require("express");
+var cookieSession = require('cookie-session');
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+var bodyParser = require("body-parser");
+var bcrypt = require('bcrypt');
 
 app.set("view engine", "ejs")
 
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'user_id',
+  secret: 'abcdefg'
+}));
 
-var urlDatabase = {
-  "userRandomID": {
+app.use(bodyParser.urlencoded({extended: true}));
+
+var urlDatabase = { // object that takes in additional URL key/values -
+  "userRandomID": { // - under an object linked to user that created
     "b2xVn2": "http://www.lighthouselabs.ca"
   },
   "user2RandomID": {
@@ -19,20 +23,8 @@ var urlDatabase = {
   }
 };
 
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-}
-
-app.use(bodyParser.urlencoded({extended: true}));
+var users = {  // when registered, users will be stored as objects within the users object
+};
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -46,138 +38,143 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
- // urlsForUser(req.cookies.user_id);  // calling function to attemp to integrate into index.
-
-  let templateVars = {
-    userURLs: urlDatabase[req.cookies.user_id],
-    user: users[req.cookies.user_id]
-   // urls: urlsForUser(req.cookies.user_id)//.req.cookies.user_id
+app.get("/urls", (req, res) => { // renders index page
+  var templateVars = {
+    userURLs: urlDatabase[req.session.user_id],
+    user: users[req.session.user_id]
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    user: users[req.cookies.user_id]  // added whole templateVars for cookies
+  var templateVars = {
+    user: users[req.session.user_id]  // added whole templateVars for cookies
   };
-  if (users[req.cookies.user_id]) {
+  if (users[req.session.user_id]) { // user cannot go to create short URLS unless signed in
     res.render("urls_new", templateVars);
   } else {
-    res.redirect("/login")
+    res.redirect("/login") // if user is not logged in and tries to click create URL, redirects to login
   }
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    user: users[req.cookies.user_id], // added line to template vars for cookies
+  var templateVars = {
     shortURL: req.params.id, //req.params.id.  id = the user input after /urls/
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.session.user_id][req.params.id],
+    user: users[req.session.user_id]
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/register", (req, res) => {
+  if (users[req.session.user_id]) {
+    res.redirect('/urls');
+  } else {
   res.render("register");
+  }
 });
 
 app.post("/register", (req, res) => {
   if (!req.body.email || !req.body.password) {  // ensuring email and password are filled out
     res.status(400).send('Must fill in email and password sections');
   }
-  for (let id in users) {
+  for (var id in users) {
     if (req.body.email == users[id].email) {  // making sure register email doesnt already exist
       return res.status(400).send('Email already exists');
     }
   }
-  const randomID = generateRandomString();  // generating random 6 digit ID for user
+  var randomID = generateRandomString();  // generating random 6 digit ID for user
   users[randomID] = {"id": randomID, "email": req.body.email, "password": bcrypt.hashSync(req.body.password, 10)};
-  res.cookie('user_id', randomID);
-  console.log(bcrypt.hashSync(req.body.password, 10));
-  res.redirect('/urls/');
-})
+  req.session.user_id = randomID;
+  res.redirect('/urls');
+});
 
 app.get("/login", (req, res) => {
+  if (users[req.session.user_id]) {
+    res.redirect('/urls');
+  } else {
   res.render("login");
+  }
 });
 
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();  // generating random 6 digit ID for inputted URL
-  if (!urlDatabase[req.cookies.user_id]) {
-    urlDatabase[req.cookies.user_id] = {};
-    urlDatabase[req.cookies.user_id][shortURL] = req.body.longURL;
+  var shortURL = generateRandomString();  // generating random 6 digit ID for inputted URL
+  if (!urlDatabase[req.session.user_id]) {
+    urlDatabase[req.session.user_id] = {};
+    urlDatabase[req.session.user_id][shortURL] = req.body.longURL;
   } else {
-    urlDatabase[req.cookies.user_id][shortURL] = req.body.longURL;
+    urlDatabase[req.session.user_id][shortURL] = req.body.longURL;
   }
   res.redirect(`urls/${shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
+  if (!findUserID(req.params.shortURL)) {
+    res.status(404).send('Invalid short URL')
+  } else {
   var fullURL = findUserID(req.params.shortURL);
   res.redirect(fullURL);
+  }
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.cookies.user_id][req.params.id]; // used to be urlDatabase.[req.params.id]
-  res.redirect("/urls/");
+  delete urlDatabase[req.session.user_id][req.params.id]; // used to be urlDatabase.[req.params.id]
+  res.redirect("/urls");
 });
 
-app.post("/urls/:id/update", (req, res) => {
-  urlDatabase[req.cookies.user_id][req.params.id] = req.body.longURL;
-  res.redirect("/urls/");
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.session.user_id][req.params.id] = req.body.longURL;
+  res.redirect("/urls");
 });
 
-
-app.post("/login", (req, res) => {
-  let userIden = findUserByEmail(req.body.email);
- if (!userIden) {
+app.post("/login", (req, res) => {  // checks if login information matches server and redirects to urls if so
+  var userIden = findUserByEmail(req.body.email);
+  if (!userIden) {
    res.status(403).send("Email is invalid!")
- } else if (!hasUserPass(req.body.email, req.body.password)) {
+  } else if (!hasUserPass(req.body.email, req.body.password)) {
    res.status(403).send("Password is invalid!")
- } else {
-   res.cookie('user_id', userIden);
+  } else {
+   req.session.user_id = userIden;
    res.redirect("/urls");
- }
+  }
 });
-
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect("/urls/");
-})
+ res.clearCookie("user_id");
+ res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
+function generateRandomString() { // generates random 6 digit string
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
   for (var i = 0; i < 6; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 }
 
-function findUserByEmail(email) {
- for (let user in users) {
-   if (users[user].email === email) {
-     return users[user].id
-   }
+function findUserByEmail(email) { //  finds the email associated with user logging in
+  for (var user in users) {
+    if (users[user].email === email) {
+     return users[user].id;
+    }
   }
 }
 
-function findUserID (URL) {
-  for (var userCode in urlDatabase) {
+function findUserID (URL) { // finds user id associated with the inputted shortURL to redirect
+  for (var userCode in urlDatabase) { // even if user is not logged in
     if (urlDatabase[userCode][URL]) {
       return urlDatabase[userCode][URL]
     }
   }
 }
 
-function hasUserPass(email, password) {
-  for (let user in users) {
+function hasUserPass(email, password) { // compares email / password to ensure login credentials
+  for (var user in users) {
     if (email == users[user].email) {
       if (bcrypt.compareSync(password, users[user].password)) {
         return true;
@@ -187,4 +184,3 @@ function hasUserPass(email, password) {
   }
   return false;
 }
-
